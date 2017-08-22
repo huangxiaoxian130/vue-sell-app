d7a4517185033e9b443ee1767f7898ee2ffd4189
huangxiaoxian130 <923401804@qq.com>
2017-08-22 17:18:08 +0800

    提交文件

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <headerWrap></headerWrap>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      I am content
+    </div>
+  </div>
+</template>
+
+<script>
+  import headerWrap from './components/Header/Header.vue'
+  export default {
+    name: 'app',
+    components: {
+      headerWrap
+    }
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  #app {
+    .tab {
+      display: flex;
+      width: 100%;
+      height: 40px;
+      line-height: 40px;
+      .tab-item {
+        flex: 1;
+        text-align: center;
+      }
+    }
+  }
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header-wrap">
+    我是header
+    <h1>111</h1>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default{
+    name: 'header-wrap'
+  }
+
+</script>
+<style lang="scss">
+
+</style>
